f8a7294c4e49eeaea0c49660282d3cd6e3a6ccc3
JRossMorgan <jedmorgam@hotmail.com>
2024-01-06 16:30:11 -0800

    6th commit

--- app/src/main/java/com/termsapp/Pages/AddAssessments.java
+++ app/src/main/java/com/termsapp/Pages/AddAssessments.java
@@ -3,14 +3,43 @@ package com.termsapp.Pages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.termsapp.R;
 
-public class AddAssessments extends AppCompatActivity {
+import java.time.LocalDateTime;
 
+import Database.Repository;
+
+public class AddAssessments extends AppCompatActivity {
+    String title;
+    LocalDateTime end;
+    String type;
+    EditText assessmentTitle;
+    TextView assessmentDate;
+    RadioButton objective;
+    RadioButton performance;
+    Repository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assessments);
+        repository = new Repository(getApplication());
+        title = getIntent().getStringExtra("Title");
+        assessmentTitle = findViewById(R.id.assessmentTitle);
+        assessmentTitle.setText(title);
+        objective = findViewById(R.id.assessmentO);
+        performance = findViewById(R.id.assessmentP);
+        if(objective.isSelected()){
+            type = "Objective";
+        }
+        else if(performance.isSelected()){
+            type = "Performance";
+        }
+        end = LocalDateTime.parse(getIntent().getStringExtra("End Date"));
+        assessmentDate = findViewById(R.id.dateView);
+        assessmentDate.setText(end.toString());
     }
 }
